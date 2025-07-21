2e960b8220ab6fe5c0212df0c8a8fc8ded9489e2
Petr Porobov <porobov@users.noreply.github.com>
2025-07-21 18:21:44 +0400

    Moving website to mainnet

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,8 +1,9 @@
-export const NETWORK = "sepolia"; // Change to 'sepolia' or 'mainnet' as needed
+export const NETWORK = "mainnet"; // Change to 'sepolia' or 'mainnet' as needed
 
 export const PROVIDERS = {
   local: "http://localhost:8545"
 };
 
 // Add The Graph subgraph endpoint placeholder
-export const GRAPH_API_URL = 'https://api.studio.thegraph.com/query/2388/ww-sepolia-1/version/latest';
+// export const GRAPH_API_URL = 'https://api.studio.thegraph.com/query/2388/ww-sepolia-1/version/latest';
+export const GRAPH_API_URL = 'https://api.studio.thegraph.com/query/2388/ww-mainnet/version/latest';
