4aa37d2894e39dd606476034a7ffd25089c5ae9f
Petr Porobov <porobov@users.noreply.github.com>
2025-07-22 14:28:13 +0400

    Graph published

--- docs/js/config.js
+++ docs/js/config.js
@@ -6,4 +6,5 @@ export const PROVIDERS = {
 
 // Add The Graph subgraph endpoint placeholder
 // export const GRAPH_API_URL = 'https://api.studio.thegraph.com/query/2388/ww-sepolia-1/version/latest';
-export const GRAPH_API_URL = 'https://api.studio.thegraph.com/query/2388/ww-mainnet/version/latest';
+// export const GRAPH_API_URL = 'https://api.studio.thegraph.com/query/2388/ww-mainnet/version/latest';
+export const GRAPH_API_URL = 'https://gateway.thegraph.com/api/aa0a71c836b11673988618ec8aac3db6/subgraphs/id/DpFES8AmD348YgF2mBZHK1NwAGyWs6AeKscxS43LhDg5';
